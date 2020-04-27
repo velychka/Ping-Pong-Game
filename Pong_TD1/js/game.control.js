e28ab89428e065d6a7c1a10aecf0dc272008f8d4
velychka <nastyavelychko@gmail.com>
2020-04-27 19:54:37 +0200

    TD1

---
+++ Pong_TD1/js/game.control.js
@@ -0,0 +1,77 @@
+game.control = {
+
+
+    controlSystem : null,
+    mousePointer : null,
+
+    onStartGameClickButton : function() {
+        
+        if ( !game.gameOn ) {
+            
+        
+          game.reinitGame();
+          game.gameOn = true;
+          
+        }
+    },
+   
+    onKeyDown : function(event) {
+
+        game.control.controlSystem = "KEYBOARD";
+        if ( event.keyCode == game.keycode.KEYDOWN ) {
+            game.playerOne.goDown = true;
+        } 
+        else if ( event.keyCode == game.keycode.KEYUP ) {
+            game.playerOne.goUp = true;
+        }
+
+
+        if ( event.keyCode == game.keycode.SPACEBAR && !game.ball.inGame && game.gameOn ) { 
+            game.ball.inGame = true;
+            game.ball.posX = game.playerOne.posX + game.playerOne.width;
+            game.ball.posY = game.playerOne.posY;
+            game.ball.directionX = 1;
+            game.ball.directionY = 1;
+        }
+    },
+   
+    onKeyUp : function(event) {
+
+        game.control.controlSystem = "KEYBOARD";
+        if ( event.keyCode == game.keycode.KEYDOWN ) {
+            game.playerOne.goDown = false;
+        } 
+        else if ( event.keyCode == game.keycode.KEYUP ) {
+            game.playerOne.goUp = false;
+        }
+
+    },
+
+    onMouseMove : function (event) {
+
+     
+
+        game.control.controlSystem = "MOUSE";
+
+        if ( event ) {
+            game.control.mousePointer = event.clientY;
+        }
+
+        if ( game.control.mousePointer > game.playerOne.posY ) {
+            game.playerOne.goDown = true;
+            game.playerOne.goUp = false;
+        } 
+        
+        else if ( game.control.mousePointer < game.playerOne.posY ) {
+            game.playerOne.goDown = false;
+            game.playerOne.goUp = true;
+        } 
+
+        else {
+            game.playerOne.goDown = false;
+            game.playerOne.goUp = false;
+        }
+    }
+
+
+  }
---
+++ Pong_TD1/js/game.js
@@ -0,0 +1,256 @@
+var game = {
+
+
+    groundWidth : 700,
+    groundHeight : 400,
+    groundColor: "#000000",
+    netWidth : 6,
+    netColor: "#FFFFFF",
+       
+    groundLayer : null,
+    scoreLayer : null,
+    playersBallLayer : null,
+    scorePosPlayer1 : 300,
+    scorePosPlayer2 : 365,
+    wallSound : null,
+    playerSound : null,
+    divGame : null,
+    gameOn : false,
+    startGameButton : null,
+
+    ball : {
+        width : 10,
+        height : 10,
+        color : "#FFFF00",
+        posX : 200,
+        posY : 200, 
+        speed : 1,
+        directionX : 1,
+        directionY : 1,
+        inGame : false,
+
+
+        move : function() {
+            if(this.inGame){
+                
+                this.posX += this.directionX * this.speed;
+                this.posY += this.directionY * this.speed;
+            }
+        },
+
+        bounce : function(soundToPlay) {
+            if ( this.posX > game.groundWidth || this.posX < 0 ){
+
+                this.directionX = -this.directionX;
+                soundToPlay.play();
+
+            }
+            if ( this.posY > game.groundHeight || this.posY < 0  ){
+
+                this.directionY = -this.directionY;  
+                soundToPlay.play();    
+            }
+
+            
+        }, 
+
+        collide : function(anotherItem) {
+        if ( !( this.posX >= anotherItem.posX + anotherItem.width || this.posX <= anotherItem.posX - this.width
+        || this.posY >= anotherItem.posY + anotherItem.height || this.posY <= anotherItem.posY - this.height ) ) {
+            // Collision
+            return true;
+        } 
+        return false;
+        },
+
+        lost : function(player) {
+            var returnValue = false;
+            if ( player.originalPosition == "left" && this.posX < player.posX - this.width ) {
+              returnValue = true;
+            } else if ( player.originalPosition == "right" && this.posX > player.posX + player.width ) {
+              returnValue = true;
+            }
+            return returnValue;
+          }
+    },
+
+    playerOne : {
+        width : 10,
+        height : 50,
+        color : "#FFFFFF",
+        posX : 10,
+        posY : 200,
+        goUp : false,
+        goDown : false,
+        originalPosition : "left",
+        score : 0,
+        ai : false
+    },
+       
+      playerTwo : {
+        width : 10,
+        height : 50,
+        color : "#FFFFFF",
+        posX : 680,
+        posY : 200,
+        goUp : false,
+        goDown : false,
+        originalPosition : "right",
+        score : 0,
+        ai : true
+
+        
+    },
+   
+    init : function() {
+
+      this.divGame = document.getElementById("divGame");
+      this.startGameButton = document.getElementById("startGame");
+   
+      this.groundLayer= game.display.createLayer("terrain", this.groundWidth, this.groundHeight, this.divGame, 0, "#000000", 10, 50); 
+      game.display.drawRectangleInLayer(this.groundLayer, this.netWidth, this.groundHeight, this.netColor, this.groundWidth/2 - this.netWidth/2, 0);
+         
+      this.scoreLayer = game.display.createLayer("score", this.groundWidth, this.groundHeight, this.divGame, 1, undefined, 10, 50);
+      game.display.drawTextInLayer(this.scoreLayer , "SCORE", "10px Arial", "#FF0000", 10, 10);
+         
+      this.playersBallLayer = game.display.createLayer("joueursetballe", this.groundWidth, this.groundHeight, this.divGame, 2, undefined, 10, 50);  
+      game.display.drawTextInLayer(this.playersBallLayer, "JOUEURSETBALLE", "10px Arial", "#FF0000", 100, 100);
+         
+      this.displayScore(0,0);
+      this.displayBall(200,200);
+      this.displayPlayers();
+        
+      this.initKeyboard(game.control.onKeyDown, game.control.onKeyUp);
+      this.initMouse(game.control.onMouseMove);
+      
+      this.initStartGameButton();
+       
+      this.wallSound = new Audio("./sound/wall.ogg");
+      this.playerSound = new Audio("./sound/player.ogg");
+     
+      game.ai.setPlayerAndBall(this.playerTwo, this.ball);
+      
+    }, 
+
+    displayScore : function(scorePlayer1, scorePlayer2) {
+      //this.scoreLayer = game.display.createLayer("score", this.groundWidth, this.groundHeight, undefined, 1, undefined, 0, 0);
+      game.display.drawTextInLayer(this.scoreLayer, scorePlayer1, "60px Arial", "#FFFFFF", this.scorePosPlayer1, 55);
+      game.display.drawTextInLayer(this.scoreLayer, scorePlayer2, "60px Arial", "#FFFFFF", this.scorePosPlayer2, 55);
+    }, 
+
+    displayBall : function() {
+        game.display.drawRectangleInLayer(this.playersBallLayer, this.ball.width, this.ball.height, this.ball.color, this.ball.posX, this.ball.posY);
+    },
+
+    displayPlayers : function() {
+        //this.displayScore( game.playerOne.scorePlayer1, game.playerTwo.scorePlayer2);
+        game.display.drawRectangleInLayer(this.playersBallLayer, this.playerOne.width, this.playerOne.height, this.playerOne.color, this.playerOne.posX, this.playerOne.posY);
+        game.display.drawRectangleInLayer(this.playersBallLayer, this.playerTwo.width, this.playerTwo.height, this.playerTwo.color, this.playerTwo.posX, this.playerTwo.posY);
+    },
+    
+    displayBall : function() {
+        game.display.drawRectangleInLayer(this.playersBallLayer, this.ball.width, this.ball.height, this.ball.color, this.ball.posX, this.ball.posY);
+    },
+
+    moveBall : function() { 
+        this.ball.move();
+        this.ball.bounce(this.wallSound);
+        this.displayBall();
+    },
+    
+    clearLayer : function(targetLayer) {
+        targetLayer.clear();
+    }, 
+
+    initKeyboard : function(onKeyDownFunction, onKeyUpFunction) {
+        window.onkeydown = onKeyDownFunction;
+        window.onkeyup = onKeyUpFunction;
+    },
+
+    movePlayers : function() {
+
+        if ( game.control.controlSystem == "KEYBOARD" ) {
+          // keyboard control
+          if ( game.playerOne.goUp && game.playerOne.posY > 0 ) {
+            game.playerOne.posY-=5;
+          } 
+          else if ( game.playerOne.goDown && game.playerOne.posY < game.groundHeight - game.playerOne.height) {
+            game.playerOne.posY+=5;
+          }
+
+        } 
+        
+        else if ( game.control.controlSystem == "MOUSE" ) {
+          // mouse control
+          if (game.playerOne.goUp && game.playerOne.posY > game.control.mousePointer){
+              
+              game.playerOne.posY-=5;
+          }
+          else if (game.playerOne.goDown && game.playerOne.posY < game.control.mousePointer && this.playerOne.posY < game.groundHeight - this.playerOne.height){
+              
+              game.playerOne.posY+=5;
+          }
+        }
+    },
+
+    initMouse : function(onMouseMoveFunction) {
+        window.onmousemove = onMouseMoveFunction;
+    },
+
+    collideBallWithPlayersAndAction : function() { 
+        if ( this.ball.collide(game.playerOne) ){
+            game.ball.directionX = -game.ball.directionX;
+            this.playerSound.play();
+
+        }
+        if ( this.ball.collide(game.playerTwo) ){
+            game.ball.directionX = -game.ball.directionX;
+            this.playerSound.play();
+
+        }
+    },  
+
+
+    lostBall : function() {
+      if ( this.ball.lost(this.playerOne) ) {
+        this.playerTwo.score++;
+        if ( this.playerTwo.score > 9 ) {
+          this.gameOn = false;
+        } else {
+          this.ball.inGame = false;
+       
+          if ( this.playerOne.ai ) { 
+            setTimeout(game.ai.startBall(), 3000);
+          }
+        }
+      } else if ( this.ball.lost(this.playerTwo) ) {
+        this.playerOne.score++;
+        if ( this.playerOne.score > 9 ) {
+          this.gameOn = false;
+        } else {
+          this.ball.inGame = false;
+   
+          if ( this.playerTwo.ai ) { 
+            setTimeout(game.ai.startBall(), 3000);
+          }
+        }
+      }
+     
+      this.scoreLayer.clear();
+      this.displayScore(this.playerOne.score, this.playerTwo.score);
+    },
+
+      initStartGameButton : function() {;
+        this.startGameButton.onclick = game.control.onStartGameClickButton;
+      },
+
+      reinitGame : function() {
+        this.ball.inGame = false;
+        this.playerOne.score = 0;
+        this.playerTwo.score = 0;
+        this.scoreLayer.clear();
+        this.displayScore(this.playerOne.score, this.playerTwo.score);
+      },
+    
+   
+};
